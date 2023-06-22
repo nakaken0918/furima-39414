5e002bd124b95061c74d00e130527c760b386906
nakaken0918 <nakaken18.wywy@gmail.com>
2023-06-22 12:58:22 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, except:[:index]
+  before_action :authenticate_user!, except:[:index, :show]
   before_action :set_user, only: [:new, :create]
 
   def index
@@ -20,7 +20,9 @@ class ItemsController < ApplicationController
     end
   end
 
-
+  def show
+    @item = Item.find(params[:id])
+  end
 
   private
   def item_params
